db2033c16ae2c85e72ca24c33dcfc115df396a88
Antonio Lain <antlai@cafjs.com>
2014-12-07 23:28:06 -0800

    Test message processing

--- test/test-platform.js
+++ test/test-platform.js
@@ -5,6 +5,19 @@ var hello = require('./hello/main.js');
 
 var app = hello;
 
+var newMsg = function(ca, sessionId, methodName, arg1) {
+    var msg = json_rpc.systemRequest(ca, methodName);
+    if (arg1 === undefined) {
+        return json_rpc.request(json_rpc.getToken(msg), json_rpc.getTo(msg),
+                                json_rpc.getFrom(msg), sessionId,
+                                methodName);
+    } else {
+        return json_rpc.request(json_rpc.getToken(msg), json_rpc.getTo(msg),
+                                json_rpc.getFrom(msg), sessionId,
+                                methodName, arg1);
+    }
+};
+
 module.exports = {
     setUp: function (cb) {
         var self = this;
@@ -61,12 +74,15 @@ module.exports = {
 
         var self = this;
         var all = [];
-        for (var i =0; i<1000; i++) {
+        var NCAs = 1000;
+        for (var i =0; i<NCAs; i++) {
             all.push('ca'+i);
         }
-        test.expect(3);
+        test.expect(6);
+        var t1, t2, t3, t4, t5, t6;
         async.series([
                          function(cb) {
+                             t1 = (new Date()).getTime();
                              async
                                  .each(all,
                                           function(x, cb1) {
@@ -78,6 +94,9 @@ module.exports = {
                                           }, cb);
                          },
                          function(cb) {
+                             t2 = (new Date()).getTime();
+                             console.log("Time to create " + NCAs + " CAs: "
+                                         + (t2-t1));
                              test.equals(typeof self.$._.$.registry.$.ca7,
                                          'object');
                              test.equals(self.$._.$.registry.$.ca7
@@ -86,9 +105,71 @@ module.exports = {
                              setTimeout(function() {
                                             cb(null);
                                         }, 5000);
+                         },
+                         function(cb) {
+                             t3 = (new Date()).getTime();
+                             async
+                                 .each(all,
+                                          function(x, cb1) {
+                                              var ca = self.$._.$.registry.$[x];
+                                              var msg = newMsg(x, 'default',
+                                                               'hello', 'hi');
+                                              async.series([
+                                                               function(cb2) {
+                                                                   ca.__ca_process__(msg, cb2);
+                                                               },
+                                                               function(cb2) {
+                                                                   ca.__ca_process__(msg, cb2);
+                                                               },
+                                                               function(cb2) {
+                                                                   ca.__ca_process__(msg, cb2);
+                                                               },
+                                                               function(cb2) {
+                                                                   ca.__ca_process__(msg, cb2);
+                                                               },
+                                                               function(cb2) {
+                                                                   ca.__ca_process__(msg, cb2);
+                                                               }
+                                                           ], cb1);
+                                          }, cb);
+                         },
+                         function(cb) {
+                             t4 = (new Date()).getTime();
+                             console.log("Time to process " + 5*NCAs + " msgs: "
+                                         + (t4-t3));
+                             var ca = self.$._.$.registry.$['ca99'];
+                             var msg = newMsg('ca99', 'default',
+                                              'getLastMessage');
+                             var cb1 = function(err, data) {
+                                 test.ifError(err);
+console.log(JSON.stringify(data));
+                                 var resp =
+                                     json_rpc.getAppReplyData(data);
+                                 test.equals(resp, 'hi');
+                                 cb(err, data);
+                             };
+                             ca.__ca_process__(msg, cb1);
+                         },
+
+                         function(cb) {
+                             t5 = (new Date()).getTime();
+                             async
+                                 .each(all,
+                                          function(x, cb1) {
+                                              var reg = self.$._.$.registry;
+                                              reg.__ca_deleteChild__ (null, x,
+                                                                      cb1);
+                                          }, cb);
                          }
+
                      ], function(err, res) {
+                         t6 = (new Date()).getTime();
+                          console.log("Time to delete " + NCAs + " CAs: "
+                                         + (t6-t5));
+                         test.equals(typeof self.$._.$.registry.$.ca7,
+                                     'undefined');
                          test.ifError(err);
+
                          test.done();
                      });
     }
